a526b594d90caea25382cead705a0bcffa14fde0
Raffaele Maffei <tidus.rm@libero.it>
2014-08-21 13:29:16 +0200

    1st phase: OK

--- provaGiu3.c
+++ provaGiu3.c
@@ -11,13 +11,13 @@ int main(int argc, char *argv[]) {
   float matrix[n][n]; // matrice input
   float mr[n][n]; // matrice risultato
 
-  file = fopen("./graph-gen/graph.txt", "r"); //apre il file
-  if (file==NULL) {
+  file = fopen("./graph-gen/graph.txt", "r"); // apre il file
+  if (file == NULL) {
       perror("Errore in apertura del file");
       exit(1);
   }
 
-  while (!feof(file)) {	 //legge e memorizza i valori in una matrice (array bidimensionale)
+  while (!feof(file)) {	 // legge e memorizza i valori in una matrice (array bidimensionale)
       int i = 0;
   	   int j = 0;
       for (i = 0; i < n; i++) {
@@ -27,7 +27,7 @@ int main(int argc, char *argv[]) {
 	   }
   }
   
-  fclose(file); //chiude il file
+  fclose(file); // chiude il file
   
 /* FINE OPERAZIONI DA FILE ESTERNI */
 
@@ -50,7 +50,7 @@ int main(int argc, char *argv[]) {
       printf(" *** MATRICE IMPORTATA ***\n");
 	      for (i=0; i<n; i++) {
 	  	      for (j=0; j<n; j++) {
-	         	printf("%.2f ", matrix[i][j]); //lo stampa come controllo GIUSTO
+	         	printf("%.2f ", matrix[i][j]);
 	   	   }
 		      printf("\n");
 	      }
@@ -62,8 +62,8 @@ int main(int argc, char *argv[]) {
   /* ALGORITMO PER TROVARE IL MIN SULLE RIGHE */
   // ogni processore legge la riga=rank di matrix e scrive il valore min in mr
   
-  int indexR = 0; //indice di riga
-  int indexC = 0; //indice di colonna
+  int indexR = 0; // indice di riga
+  int indexC = 0; // indice di colonna
   float min;
   int k;
   
@@ -76,64 +76,52 @@ int main(int argc, char *argv[]) {
 
   for (k = 0; k < n; k++) {
 	   if (matrix[rank][k] < min && matrix[rank][k] != 0) {
-	      min = matrix[rank][k]; 		// salvo valore
-	      indexR = rank;			// salvo riga
-	      indexC = k;			// salvo colonna	      	
+	      min = matrix[rank][k];  // salvo valore
+	      indexR = rank; // salvo riga
+	      indexC = k; // salvo colonna	      	
 	   }
   }
   
   mr[indexR][indexC] = min; //aggiorno la matrice risultato
   printf(" *** [%d] %.2f (%d,%d)\n ", rank, min, rank, indexC); // print test
   
+  /* FINE ALGORITMO */
+  
   /* SYNC */
   MPI_Barrier(MPI_COMM_WORLD);
   
-  /* COMUNICAZIONE MINIMO */
+  /* COMUNICAZIONE */
   
-  // PROBLEMA!!!
-  
-  float buffer = mr[indexR][indexC];
-  int col = indexC;
-  int raw = indexR;
+  float buffer;
+  int col;
+  int raw;
   int count = 1;
   
-  /* SYNC */
-  MPI_Barrier(MPI_COMM_WORLD);
+  int rankBcast;
   
-  printf("[%d] buffer: %.2f\n", rank, buffer);
-  printf("[%d] raw: %d\n", rank, raw);
-  printf("[%d] col: %d\n", rank, col);
+  for(rankBcast = 0; rankBcast < n; rankBcast++) {
   
-  /* SYNC */
-  MPI_Barrier(MPI_COMM_WORLD);
+      buffer = mr[indexR][indexC];
+      col = indexC;
+      raw = indexR;
   
-  // <--- prima di questo punto i buffer, raw e col sono corretti per tutti
+      MPI_Bcast(&buffer, count, MPI_FLOAT, rankBcast, MPI_COMM_WORLD);  
+      MPI_Bcast(&col, count, MPI_INT, rankBcast, MPI_COMM_WORLD);
+      MPI_Bcast(&raw, count, MPI_INT, rankBcast, MPI_COMM_WORLD);
   
-  MPI_Bcast(&buffer, count, MPI_FLOAT, 0, MPI_COMM_WORLD);  
-  MPI_Bcast(&col, count, MPI_INT, 0, MPI_COMM_WORLD);
-  MPI_Bcast(&raw, count, MPI_INT, 0, MPI_COMM_WORLD);
+      mr[raw][col] = buffer;
   
-  mr[raw][col] = buffer;
+      //printf("=== Buffer [%d]: %.2f\n", rank, buffer);
   
-  printf("=== Buffer [%d]: %.2f\n", rank, buffer);
+      /* SYNC */
+      MPI_Barrier(MPI_COMM_WORLD);
+  }
   
-  /* SYNC */
-  MPI_Barrier(MPI_COMM_WORLD);
-  
-  MPI_Bcast(&buffer, count, MPI_FLOAT, 1, MPI_COMM_WORLD);  
-  MPI_Bcast(&col, count, MPI_INT, 1, MPI_COMM_WORLD);
-  MPI_Bcast(&raw, count, MPI_INT, 1, MPI_COMM_WORLD);
-  
-  mr[raw][col] = buffer;
-  
-  printf("=== Buffer [%d]: %.2f\n", rank, buffer);
-  
-  /* SYNC */
-  MPI_Barrier(MPI_COMM_WORLD);
+  /* FINE COMUNICAZIONE */
   
   // print test
-  if (rank == 2) {
-      printf(" *** MATRICE RISULTATO ***\n");
+  if (rank == 0) {
+      printf(" *** MATRICE RISULTATO [%d] ***\n", rank);
          for(i = 0; i < n; i++) {
 	  	      for(j = 0; j < n; j++) {
 	  		      printf("%.2f ", mr[i][j]);
@@ -141,7 +129,7 @@ int main(int argc, char *argv[]) {
 	         printf("\n");
 	      }
   }
-
+  
   MPI_Finalize();
 
 } //chiude il main
